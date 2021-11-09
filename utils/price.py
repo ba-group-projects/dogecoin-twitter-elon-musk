080441194fac2aef97a8d37f3f13cca4789a950c
Benedict <yinji.zhou.2@bayes.city.ac.uk>
2021-11-09 19:47:17 +0000

    Finished price getting class

--- utils/price.py
+++ utils/price.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import ccxt
+from dateutil import parser
+from debugpy.common import timestamp
 
 
 class PriceGetter:
@@ -7,15 +9,30 @@ class PriceGetter:
         self.binance = ccxt.binance()
         self.symbol = symbol
         self.timeframe = timeframe
-        self.dfm = pd.DataFrame()
 
-    def update_price(self,since,limit):
-        ohlcv = self.binance.fetch_ohlcv(self.symbol, self.timeframe,since,limit)
-        self.dfm.append(ohlcv)
+    def rank_data(self):
+        self.dfm.sort_values("timestamp")
+
+    def rename_col(self):
+        self.dfm.rename({0: "timestamp", 1: "open", 2: "high", 3: "low", 4: "close", 5: "volume"}, inplace=True)
+
+    def update_price(self, since, limit):
+        ohlcv = pd.DataFrame(self.binance.fetch_ohlcv(self.symbol, self.timeframe, since, limit))
+        self.dfm = self.dfm.append(ohlcv)
+
+    def drop_duplicates(self):
+        self.dfm = self.dfm.drop_duplicates()
 
     def save_price(self):
-        pass
+        self.dfm.to_csv(f"data/price_clean.csv", index=False)
 
-
-last = print_chart(binance, symbol, timeframe)
-print("\n" + binance.name + " ₿ = $" + str(last) + "\n")  # print last closing price
+if __name__ == '__main__':
+    price = PriceGetter()
+    date = pd.read_csv("./data/doge_tweets.csv")["timestamp"]
+    for d in date:
+        timestamp = int(parser.parse(d).timestamp() - 3600) * 1000
+        price.update_price(since=timestamp, limit=120)
+    price.rename_col()
+    price.drop_duplicates()
+    price.rank_data()
+    price.save_price()
